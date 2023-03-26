f249e5dacf997dc9892de5811e229c7475ef6384
InTTeY <123205539+InTTeY@users.noreply.github.com>
2023-03-26 19:03:17 +0300

    Add files via upload

---
+++ PW3_4/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace PW3_4
+{
+    class MyintList
+    {
+        private List<int> numberList = new List<int>();
+        public double Average
+        {
+            get
+            {
+                return CalculateAveragre();
+            }
+        }
+        public void AddNumber(int number)
+        {
+            numberList.Add(number);
+        }
+        public void RemoveNumber(int number)
+        {
+            numberList.Remove(number);
+        }
+        private double CalculateAveragre()
+        {
+            int sum = 0;
+            foreach (int number in numberList)
+            {
+                sum += number;
+            }
+            return sum / numberList.Count;
+        }
+        
+        // добавлено свойство и метод //
+        public double GeometricMean
+        {
+            get
+            {
+                return CalculateGeometricMean();
+            }
+        }
+        private double CalculateGeometricMean()
+        {
+            int proizv = 1;
+            foreach (int number in numberList)
+            {
+                proizv *= number;
+            }
+            return Math.Pow(proizv, 1.0 / numberList.Count);
+        }
+
+        // добавлено свойство и метод //
+        public double TotalSum
+        {
+            get
+            {
+                return CalculateTotalSum();
+            }
+        }
+        private double CalculateTotalSum()
+        {
+            int sum = 0;
+            foreach (int number in numberList)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+    class programm
+    {
+        static void Main()
+        {
+            MyintList numbers = new MyintList();
+            numbers.AddNumber(1);
+            numbers.AddNumber(2);
+            numbers.AddNumber(8);
+            numbers.AddNumber(16);
+            numbers.AddNumber(20);
+            numbers.AddNumber(38);
+
+            Console.WriteLine("Сред арифм: " + numbers.Average);
+            Console.WriteLine("Сред геометр: " + numbers.GeometricMean);
+            Console.WriteLine("Общая сумма: " + numbers.TotalSum);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ PW3_5/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace PW3_5
+{
+    class SmsMessage
+    {
+        private string Messagetext;
+        public int MaxLength;
+        public double Price;
+        public double SymbolTax;
+        public SmsMessage(int MaxLength, double Price, double SymbolTax, string Messagetext)
+        {
+            this.MaxLength = MaxLength;
+            this.Price = Price;
+            this.SymbolTax = SymbolTax;
+            this.Messagetext = Messagetext;
+        }
+        public string smstext
+        {
+            get { return Messagetext; }
+            set { Messagetext = Normalizesms(value); }
+        }
+        public string Normalizesms(string value)
+        {
+            if (Messagetext.Length <= 65)
+            {
+                Console.WriteLine($"Цена за сообщение - {Price} руб.");
+            }
+            else
+            {
+                if (Messagetext.Length <= MaxLength && Messagetext.Length > 65)
+                {
+                    int extraSymbols = Messagetext.Length - 65;
+                    double tax = extraSymbols * SymbolTax;
+                    double PriceTax = Price + tax;
+                    Console.WriteLine($"Цена за сообщение - {PriceTax} руб.(в сумму входят: стоимоть отправки сообщения - {Price} руб.; стоимость за превышения лимитов по символам сообщения - символы после лимита {extraSymbols}*{SymbolTax}руб.(за 1 символ) = {tax})");
+                }
+                else
+                {
+                    int length = Messagetext.Length;
+                    Console.WriteLine($"Сообщение не отправлено, так как превышен лимит символов{MaxLength}. Символов в сообщении: {length}");
+                }
+            }
+            return value;
+        }
+        public void SendInfo()
+        {
+            Console.WriteLine($"{Normalizesms(Messagetext)}");
+        }
+    }
+    class programm
+    {
+        static void Main()
+        {
+            Console.Write("Установка параметров SMS\nВведите максимальную длину сообщения(от 66 символов): ");
+            int MaxLength = Convert.ToInt32(Console.ReadLine());
+            if (MaxLength > 65)
+            {
+                Console.Write("Введите стоимость базового сообщения(за 65 символов): ");
+                double Price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите цену за 1 символ после лимита по базовому сообщению: ");
+                double SymbolTax = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nОтправка SMS\nВведите текст: ");
+                string Messagetext = Console.ReadLine();
+                SmsMessage message = new SmsMessage(MaxLength, Price, SymbolTax, Messagetext);
+
+                message.SendInfo();
+
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine($"Вы ввели неправильное значение или значение, которое меньше минимального. Введенное значение {MaxLength}");
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
